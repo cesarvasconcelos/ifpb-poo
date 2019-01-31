82cc8b79b5d815e603c1a866a2cb1ff50434a732
Cesar Rocha <cesarvasconcelos@copin.ufcg.edu.br>
2019-01-31 16:33:55 -0300

    Incluir exemplo mínimo Apache Ant
    Neste exemplo mínimo executável, é possível compilar arquivos
    *.java a partir de um diretório inicial com o build.xml,
    criar uma estrutura de diretórios necessária para o projeto,
    gerar documentação a partir códigos-fonte, limpar diretórios
    temporários, gerar um JAR executável e, por fim, rodar o programa.

---
+++ ant/projeto/src/br/edu/ifpb/Outro.java
@@ -0,0 +1,5 @@
+package br.edu.ifpb;
+
+public class Outro {
+	static int i = 77;
+}
---
+++ ant/projeto/src/br/edu/ifpb/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.edu.ifpb;
+
+/**
+* Esta é a primeira versão.
+* @author Cesar
+* @version 1.0
+*/
+public class PrimeiroPrograma {
+
+   public static void main( String args[] )
+   {
+		Outro.i = 99;
+		
+		for( int i = 0; i < 10 ; i++ )
+			System.out.println( "i= " + i  );
+   }
+}
